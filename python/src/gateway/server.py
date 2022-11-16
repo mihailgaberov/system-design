0573971f9e665e7ecd802b479a544e4f26238104
Mihail Gaberov <mihail.gaberov@olx.com>
2022-11-16 16:44:54 +0100

    🧑🏻‍💻 Building the gateway parts

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -27,3 +27,19 @@ def login():
 @server.route("/upload", methods=["POST"])
 def upload():
     access, err = validate.token(request)
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        if len(request.files) > 1 or len(request.files) < 1:
+            return "exactly 1 file required", 400
+
+        for _, f in request.files.items():
+            err = util.upload(f, fs, channel, access)
+
+            if err:
+                return err
+        
+        return "success!", 200
+    else:
+        return "not authorized", 401
